7be9e6199647e9623802cce2ba97b6c767a53086
leonardo <leotrujillo@vera.com.uy>
2018-03-21 23:10:34 -0300

    se agrega archivo según documentación http://docs.mapasculturais.org/mc_developer_i18n/

--- src/protected/application/themes/BaseV1/assets/vendor/select2_locale_es_ES-edit.js
+++ src/protected/application/themes/BaseV1/assets/vendor/select2_locale_es_ES-edit.js
@@ -5,11 +5,11 @@
     "use strict";
 
     $.extend($.fn.select2.defaults, {
-        formatNoMatches: function () { return "Nenhum resultado encontrado"; },
-        formatInputTooShort: function (input, min) { var n = min - input.length; return "Informe ao menos mais " + n + " caractere" + (n == 1? "" : "s"); },
-        formatInputTooLong: function (input, max) { var n = input.length - max; return "Apague " + n + " caractere" + (n == 1? "" : "s"); },
-        formatSelectionTooBig: function (limit) { return "Só é possível selecionar " + limit + " elemento" + (limit == 1 ? "" : "s"); },
-        formatLoadMore: function (pageNumber) { return "Carregando mais resultados..."; },
+        formatNoMatches: function () { return "No se ha encontrado ningún resultado"; },
+        formatInputTooShort: function (input, min) { var n = min - input.length; return "Informe al menos " + n + " caracter" + (n == 1? "" : "es"); },
+        formatInputTooLong: function (input, max) { var n = input.length - max; return "Borre " + n + " caracter" + (n == 1? "" : "es"); },
+        formatSelectionTooBig: function (limit) { return "Solo es posible seleccionar " + limit + " elemento" + (limit == 1 ? "" : "s"); },
+        formatLoadMore: function (pageNumber) { return "Cargando más resultados..."; },
         formatSearching: function () { return "Buscando..."; }
     });
 })(jQuery);
